5a6e8abd7193dcc288e99b783b24380c4a484ac5
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2023-12-20 17:58:08 +0630

    -

---
+++ DotNet8MediatR.User/UserModularService.cs
@@ -0,0 +1,15 @@
+﻿using DotNet8MediatR.User.Features.User;
+
+namespace DotNet8MediatR.Atm
+{
+    public static class UserModularService
+    {
+        public static IServiceCollection AddAtmModularService(this IServiceCollection services)
+        {
+            services.AddScoped<BlogDataAccess>();
+            services.AddScoped<BlogBusinessLogic>();
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(UserHandler).Assembly));
+            return services;
+        }
+    }
+}
